d27fd152a82b0bdc1421ac74b92ccb7654ba8dd6
sulvund <34556261+sulvund@users.noreply.github.com>
2021-04-15 22:52:50 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  Navbar,
+  Form,
+  Col,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand href="#home">monoReddit</Navbar.Brand>
+          <Form>
+            <Form.Row>
+              <Col>
+                <FormControl type="text" placeholder="Search" className=""></FormControl>
+              </Col>
+              <Col>
+                <Button variant="outline-dark">S</Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Navbar>
     </div>
   );
 }
