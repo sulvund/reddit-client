0cad9fdc03d044e92781ba4561074363cd5ccfc3
sulvund <34556261+sulvund@users.noreply.github.com>
2021-04-15 23:09:15 +0200

    Navigationbar to separate file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from "react";
-import {
-  Container,
-  Navbar,
-  Form,
-  Col,
-  FormControl,
-  Button,
-} from "react-bootstrap";
 import "./App.css";
+import { Navigationbar } from './components/Navigationbar'
 
 function App() {
   return (
     <div>
-      <Navbar id="navbar" bg="light">
-        <Container>
-          <Navbar.Brand href="#home">monoReddit</Navbar.Brand>
-          <Form>
-            <Form.Row>
-              <Col>
-                <FormControl type="text" placeholder="Search" className=""></FormControl>
-              </Col>
-              <Col>
-                <Button variant="outline-dark">
-                  <i class="bi bi-search"></i>
-                </Button>
-              </Col>
-            </Form.Row>
-          </Form>
-        </Container>
-      </Navbar>
+      <Navigationbar />
     </div>
   );
 }
---
+++ src/components/Navigationbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  Container,
+  Navbar,
+  Form,
+  Col,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+export const Navigationbar = () => (
+  <Navbar id="navbar" bg="light">
+    <Container>
+      <Navbar.Brand href="#home">monoReddit</Navbar.Brand>
+      <Form>
+        <Form.Row>
+          <Col>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className=""
+            ></FormControl>
+          </Col>
+          <Col>
+            <Button variant="outline-dark">
+              <i class="bi bi-search"></i>
+            </Button>
+          </Col>
+        </Form.Row>
+      </Form>
+    </Container>
+  </Navbar>
+);
