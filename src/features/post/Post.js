3ba120d8908f177a5f9ef5982784026b1d528a6f
sulvund <34556261+sulvund@users.noreply.github.com>
2021-04-22 20:40:14 +0200

    add support for differen message types

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -5,14 +5,64 @@ export const Post = ({ post }) => {
     const now = new Date();
     const createdHours = (now - created) / (1000 * 60 * 60)
 
+    let content;
+    switch (post.post_hint) {
+        case 'image':
+            content = (<img src={post.url} alt={post.title}></img>)
+            break;
+        case 'link':
+            let link;
+            if (post.url.length > 40) {
+                link = `${post.url.substring(0,40)}...`;
+            } else {
+                link = post.url;
+            }
+
+            /* let thumbnail;
+            if (post.thumbnail) {
+                thumbnail = <img className='thumnail' src={post.thumbnail} alt=''></img>;
+            } */
+
+            content = (
+                <>
+                    {<img className='thumnail' src={post.thumbnail} alt=''></img>}
+                    <a href={post.url}><p>{link}</p></a>
+                </>
+            )
+            break;
+        case 'hosted:video':
+            content = (
+                <video width="100%" height="auto" controls>
+                    <source src={post.media.reddit_video.scrubber_media_url} type="video/mp4"></source>
+                </video>
+            )
+            break;
+        case 'rich:video':
+            content = (
+                <iframe
+                    width="560"
+                    height="315"
+                    src={post.url_overridden_by_dest}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen>
+                </iframe>
+            )
+            break;
+        case 'self':
+            content = (<p >{post.selftext}</p>)
+            break;
+        default: 
+            content = (<p>{post.selftext}</p>)
+    }
+
     return (
     <div className='post'>
         <p className='subreddit'>{post.subreddit_name_prefixed}</p>
         <h5>{post.title}</h5>
         <div className='content'>
-            <img src={post.url} alt={post.title}>
-
-            </img>
+            {content}
         </div>
 
         <div className='flex-row'>
